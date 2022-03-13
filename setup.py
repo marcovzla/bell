7fe842a857b6d02d2c2716bc7d111cd20e2c3b91
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2022-03-13 13:39:44 -0700

    description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="bell",
     version="1.0",
-    description="TODO",
+    description="Ring a bell.",
     scripts=["bell.py"],
     install_requires=["pygame==2.1.2"],
 )
