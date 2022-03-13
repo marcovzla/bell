d545cdd3cad88cae3d400faec6bf72976f28f9bc
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2022-03-13 14:03:12 -0700

    url

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="bell",
     version="1.0",
     description="Ring a bell.",
+    url="https://github.com/marcovzla/bell",
     scripts=["bell"],
     install_requires=["pygame==2.1.2"],
 )
